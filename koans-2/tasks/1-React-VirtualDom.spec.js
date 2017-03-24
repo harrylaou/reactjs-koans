142ca2b1c4bf13b0e84d4016d67c214002d7f558
alexlbr <alex.lobera@gmail.com>
2017-03-24 21:10:17 +0000

    reedited questions koans 2

--- koans-2/tasks/1-React-VirtualDom.spec.js
+++ koans-2/tasks/1-React-VirtualDom.spec.js
@@ -2,31 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
 import { expect } from 'chai'
-import TodoList,{ ListItem } from '../../koans-1/tasks/2-React-TodoList-one-way-data-binding.js'
+import TodoList,{ ListItem } from '../../koans-1/tasks/Task_2'
 
-describe('Virtual DOM test. React Compoment, React Instance and React Element:', () => {
-  
-  it('JsX creates React Elements.', () => {
-    let am_I_a_component = <TodoList/>
-    
+describe('React Compoment, React Instance and React Element:', () => {
+
+  it('JSX creates React Elements.', () => {
+    const am_I_a_component = <TodoList/>
+
     // change this var
-    let change_me = '????'
+    const __CHANGE_ME__ = '????'
 
     // hint, uncomment next line to see the object in the console:
     // console.log(am_I_a_component)
 
-    expect(`Symbol(${change_me})`).to.be.equal(am_I_a_component['$$typeof'].toString())
+    expect(`Symbol(${__CHANGE_ME__})`).to.be.equal(am_I_a_component['$$typeof'].toString())
   })
 
-  it('JsX creates new objects every time.', () => {
-    let component_1 = <TodoList/>
-    let component_2 = <TodoList/>
-    
+  it('Does JSX create new react elements every time?.', () => {
+    const component_1 = <TodoList/>
+    const component_2 = <TodoList/>
+
     // change this assertion, are the two React Elements deeply equal?
     expect(component_1).not.to.be.deep.equal(component_2)
   })
 
-  it('React Elements do not implement component\'s functionality.', () => {
+  it('Do React elements implement the functionality of the component?.', () => {
     let reactElement = <TodoList/>
 
     // hint, uncomment next line to see the object in the console:
@@ -37,9 +37,9 @@ describe('Virtual DOM test. React Compoment, React Instance and React Element:',
       throw new Error('The typeof(reactElement.addTask) is not correct')
     }
   })
-  
+
   it('Rendering into the document should return a React Instance.', () => {
-    let component = TestUtils.renderIntoDocument(<TodoList/>)
+    const component = TestUtils.renderIntoDocument(<TodoList/>)
 
     // hint, uncomment next line to see the object in the console:
     // console.log('--------->',component)
@@ -52,18 +52,18 @@ describe('Virtual DOM test. React Compoment, React Instance and React Element:',
   })
 
   it('TestUtils.renderIntoDocument should return a detached React Instance.', () => {
-    let detachedComp_1 = TestUtils.renderIntoDocument(<TodoList/>)
-    let detachedComp_2 = TestUtils.renderIntoDocument(<TodoList/>)
+    const detachedComp_1 = TestUtils.renderIntoDocument(<TodoList/>)
+    const detachedComp_2 = TestUtils.renderIntoDocument(<TodoList/>)
 
     if (detachedComp_1 === detachedComp_1) {
       throw new Error('Are you sure detachedComp_1 and detachedComp_2 are the same instance?')
     }
   })
-  
+
   it('ReactDOM.render should return the same React Instance for the same component (they are not detached).', () => {
-    let component_1 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
-    let component_2 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
-    
+    const component_1 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
+    const component_2 = ReactDOM.render(<TodoList/>, document.getElementById('app'))
+
     if (component_1 === component_2) {
       throw new Error('Are you sure component_1 and component_2 are the same instance?')
     }
